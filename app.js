abdb5d8094d097b758c461106a23d20156860f79
user <user@MacBook-Pro.local>
2020-08-25 15:14:44 +0200

    update

--- app.js
+++ app.js
@@ -17,19 +17,8 @@ const db = low(adapter);
 const Mux = require("@mux/mux-node");
 const { Video, Data } = new Mux();
 
-var whitelist = ['http://2am.tv/', 'http://localhost:8080', 'https://2am.tv/', 'https://mux.com/', 'https://2am.tv', 'https://2am.tv/admin', 'https://2am.tv/login', 'https://2am.netlify.com/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    console.log('origin: ' + origin)
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 const server = app.listen(PORT, function() {
   console.log(`Listening on Port ${PORT}`);
